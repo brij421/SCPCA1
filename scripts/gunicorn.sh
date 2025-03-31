a428dc86fa9cb29bb4fbedcae8a4675e8291ac54
brijesh girish Makwana <x23220775@student.ncirl.ie>
2025-03-31 23:47:23 +0100

    final changes

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-sudo cp /home/ubuntu/SCPCA1/gunicorn/gunicorn.socket  /etc/systemd/system/gunicorn.socket
-sudo cp /home/ubuntu/SCPCA1/gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
+sudo cp /mnt/c/SCP/SCPCA1/gunicorn/gunicorn.socket  /etc/systemd/system/gunicorn.socket
+sudo cp /mnt/c/SCP/SCPCA1/gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
 
 sudo systemctl start gunicorn.service
 sudo systemctl enable gunicorn.service
--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,7 +4,7 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/SCPCA1/nginx/nginx.conf /etc/nginx/sites-available/blog
+sudo cp /mnt/c/SCP/SCPCA1/nginx/nginx.conf /etc/nginx/sites-available/blog
 sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
